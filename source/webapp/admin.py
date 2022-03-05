a77cf2477d6ec94a412a548f1613a35b9ebf9f27
sheirenov <ruslansheirenov@gmail.com>
2022-03-05 13:45:18 +0600

    Создана модель продуктов и прогнана миграция

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Product
+
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    search_fields = ['title']
+    fields = ['title', 'description', 'category', 'picture']
+
+admin.site.register(Product, ProductAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    CATEGORY_CHOICES = [('other', 'Разное'), ('TVs', 'Телевизоры'), ('notebooks', 'Ноутбуки')]
+    title = models.CharField(max_length=100, null=False, blank=False, verbose_name='Название')
+    category = models.CharField(max_length=11, choices=CATEGORY_CHOICES, default='other', verbose_name='Категория')
+    description = models.TextField(max_length=2000, null=False, blank=False, verbose_name='Описание')
+    picture = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Картинка')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
